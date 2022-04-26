935a8b5ab8462784e2ed7cbe81122bba5a273530
Iberi Daniel <danieliberi99@gmail.com>
2022-04-26 17:01:07 +0100

    halfway done. added picker and created styles for text

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location'
 import { useEffect, useState } from 'react';
 
+import WeatherInfo from './components/WeatherInfo';
+import UnitsPicker from './components/UnitsPicker';
+
+const WEATHER_API_KEY = '8cdf6f57a6d28011fb594076a8934b92'
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+
 export default function App() {
 
   const [errorMessage, setErrorMessage] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [unitSystem, setUnitSystem] = useState('metric')
 
   useEffect(() => {
     load()
@@ -15,28 +24,62 @@ export default function App() {
     try {
       let { status } = await Location.requestPermissionsAsync()
 
-      if (status! 'granted') {
+      if (status != 'granted') {
         setErrorMessage('Access to location required to run')
         return
       }
+      const location = await Location.getCurrentPositionAsync()
+
+      const { latitude, longitude } = location.coords
+
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`
+
+      // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+      const response = await fetch(weatherUrl) 
+
+      const result = await response.json()
+
+      if (response.ok) {
+        setCurrentWeather(result)
+      } else {
+        setErrorMessage(result.message)
+      }
+
     } catch (error) {
-      
+      setErrorMessage(error.message)
     }
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if (currentWeather) {
+    const { main: { temp } } = currentWeather
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <UnitsPicker />
+          <WeatherInfo currentWeather={currentWeather} />
+        </View>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  main: {
+    justifyContent: 'center',
+    flex: 1,
+  }
 });
